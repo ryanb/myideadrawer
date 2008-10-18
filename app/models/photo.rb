b37446e64cc8cef22ca513dbec461c1943c16d64
Ryan Bates <ryan@railscasts.com>
2008-10-18 12:52:58 -0700

    adding tiny version of photo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,7 +3,7 @@ class Photo < ActiveRecord::Base
   
   belongs_to :project
   
-  has_attached_file :image, :styles => { :small => "150x150>" },
+  has_attached_file :image, :styles => { :small => "150x150>", :tiny => "64x64>" },
     :url => "/assets/photos/:id/:style/:basename.:extension",
     :path => "#{RAILS_ROOT}/public/assets/photos/:id/:style/:basename.:extension" 
   
