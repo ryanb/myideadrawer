dda5cbf555541172a0e7a2cd03e8babd40a2d4f2
Ryan Bates <ryan@railscasts.com>
2008-10-18 09:26:50 -0700

    copying over production.sqlite3 database on deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,11 +27,13 @@ namespace :deploy do
   task :symlink_extras do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
     run "ln -nfs #{shared_path}/config/app_config.yml #{release_path}/config/app_config.yml"
+    run "ln -nfs #{shared_path}/db/production.sqlite3 #{release_path}/db/production.sqlite3"
   end
 
   desc "Setup shared directory."
   task :setup_shared do
     run "mkdir #{shared_path}/config"
+    run "mkdir #{shared_path}/db"
     put File.read("config/examples/database.yml"), "#{shared_path}/config/database.yml"
     put File.read("config/examples/app_config.yml"), "#{shared_path}/config/app_config.yml"
     puts "Now edit the config files and fill assets folder in #{shared_path}."
