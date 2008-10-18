7775e5de4891a2c63b2c7a5815b9ce102f503b14
Ryan Bates <ryan@railscasts.com>
2008-10-17 18:24:31 -0700

    adding user to projects association

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ActiveRecord::Base
+  belongs_to :user
   has_many :notes
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :projects
+  
   # new columns need to be added here to be writable through mass assignment
   attr_accessible :username, :email, :password, :password_confirmation
   
