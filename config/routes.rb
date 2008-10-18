854470441bbef7a555202e0f5114b0a2f0c55d9f
Kelli Shaver <kelli@kellishaver.com>
2008-10-17 21:52:35 -0400

    adding home route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ ActionController::Routing::Routes.draw do |map|
   map.signup 'signup', :controller => 'users', :action => 'new'
   map.logout 'logout', :controller => 'sessions', :action => 'destroy'
   map.login 'login', :controller => 'sessions', :action => 'new'
-  
+  map.home 'home', :controller => 'projects', :action => 'index'
   map.resources :sessions
   map.resources :users
   map.resources :notes
