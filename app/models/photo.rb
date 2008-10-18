0d9de0d7ac8c8e4a82a4f6e0b55c936ddf92da0a
Ryan Bates <ryan@railscasts.com>
2008-10-18 12:46:07 -0700

    adding jpeg library to determine pixel dimensions of image

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -10,4 +10,23 @@ class Photo < ActiveRecord::Base
   def to_s
     name
   end
+  
+  def image_width(style = nil)
+    image_jpeg(style).width
+  end
+  
+  def image_height(style = nil)
+    image_jpeg(style).height
+  end
+  
+  def image_size(style = nil)
+    [image_width(style), image_height(style)].join('x')
+  end
+  
+  private
+  
+  def image_jpeg(style = nil)
+    @jpegs ||= Hash.new
+    @jpegs[style] ||= JPEG.new(image.path(style))
+  end
 end
