ac30c8cb48619f2ad77152640e9f42416b7ff909
Ryan Bates <ryan@railscasts.com>
2008-10-19 09:15:51 -0700

    adding dependent option to associations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,12 +1,12 @@
 class Project < ActiveRecord::Base
   belongs_to :user
-  has_many :notes
-  has_many :activities
-  has_many :links
-  has_many :code_snippets
-  has_many :palettes
-  has_many :screenshots
-  has_many :photos
+  has_many :activities, :dependent => :nullify
+  has_many :notes, :dependent => :destroy
+  has_many :links, :dependent => :destroy
+  has_many :code_snippets, :dependent => :destroy
+  has_many :palettes, :dependent => :destroy
+  has_many :screenshots, :dependent => :destroy
+  has_many :photos, :dependent => :destroy
   
   generate_unique :token
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  has_many :projects
-  has_many :activities
+  has_many :projects, :dependent => :destroy
+  has_many :activities, :dependent => :nullify
   
   # new columns need to be added here to be writable through mass assignment
   attr_accessible :username, :email, :password, :password_confirmation, :openid_url
